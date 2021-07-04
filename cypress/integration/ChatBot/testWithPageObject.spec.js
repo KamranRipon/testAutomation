dcdea4160945b9042e13599f1367e3e798d12811
riponmd <mdabdullahalkamran.ripon@dataport.de>
2021-07-04 23:02:18 +0200

    riponMd/Cypress

---
+++ cypress/integration/ChatBot/testWithPageObject.spec.js
@@ -0,0 +1,31 @@
+
+import { onChatAre } from "../../support/page_objects/chatAreaPage"
+
+describe ('dChatbot Test Suit', () => {
+
+    beforeEach('Open Chat page', () => {
+
+        cy.clearCookies()
+        cy.visit('/') 
+    })
+
+    it('Chat area testing', () => {
+        
+        onChatAre.titleOfThePage()
+        //onChatAre.chatLayoutPage()
+    })
+
+    it('Verification of the Browser API Calls', () => {
+        onChatAre.testBackend()
+    })
+
+    it('Test Feedback', () => {
+        onChatAre.testFeedback()
+    })
+
+    it('Test of Viewport', () => {
+        onChatAre.testViewport()
+
+    })
+
+})
